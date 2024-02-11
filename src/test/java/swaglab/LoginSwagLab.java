3bf063183e97d184762bbacf95bb707a0e2ceecb
Vinoth-Velayutham <vvinoth101995@gmail.com>
2024-02-11 12:33:53 +0530

    updated the swag labs

--- src/test/java/swaglab/LoginSwagLab.java
+++ src/test/java/swaglab/LoginSwagLab.java
@@ -1,6 +1,9 @@
 package swaglab;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -29,15 +32,47 @@ public class LoginSwagLab {
 
 		WebElement usernameField = driver.findElement(By.id("user-name"));
 		usernameField.sendKeys("standard_user");
-		
+
 		WebElement passwordField = driver.findElement(By.id("password"));
 		passwordField.sendKeys("secret_sauce");
-		
+
 		WebElement loginButton = driver.findElement(By.id("login-button"));
 		loginButton.click();
 
+		List<WebElement> prodTitle = driver.findElements(By.xpath("//div[@class='inventory_item_name ']"));
+		System.out.println("===================================================");
+		System.out.println("The total number of products - " + prodTitle.size());
+		System.out.println("===================================================");
+		System.out.println("List of products --");
+
+		for (WebElement produ : prodTitle) {
+			System.out.println(produ.getText());
+		}
+		System.out.println("===================================================");
+		List<WebElement> anchorTaglist = driver.findElements(By.xpath("//a"));
+
+		for (WebElement tag : anchorTaglist) {
+			System.out.println("The Hyper Links is - " + tag.getAttribute("href"));
+		}
+		System.out.println("===================================================");
+
+		WebElement footerSection = driver.findElement(By.xpath("//div[@class='footer_copy']"));
+
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+
+		jse.executeScript("arguments[0].scrollIntoView()", footerSection);
+		Thread.sleep(3000);
+
+		System.out.println("===================================================");
+		WebElement backBag = driver.findElement(By.xpath("//div[text()='Sauce Labs Backpack']"));
+		backBag.click();
+
 		Thread.sleep(5000);
 		driver.quit();
+		System.out.println("===================================================");
+		// xpath Syntax - //tagname[@attribute='value']
+		// text - //div[text()='Sauce Labs Backpack']
+
 	}
 
 }
